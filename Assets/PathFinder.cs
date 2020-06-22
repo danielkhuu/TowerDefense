e07d5e6ed3e347a13feda7514a37d3cc624f3828
Daniel Khuu <danieldkhuu@gmail.com>
2020-06-22 18:22:45 -0500

    6_TD_UN2 Pathfinder script

---
+++ Assets/PathFinder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+    [SerializeField] Waypoint startWaypoint, endWaypoint;
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+
+    void Start()
+    {
+        LoadBlocks();
+        ColorStartAndEnd();
+    }
+
+    void ColorStartAndEnd()
+    {
+        startWaypoint.SetTopColor(Color.green);
+        endWaypoint.SetTopColor(Color.red);
+    }
+
+    void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPos();
+            if (grid.ContainsKey(gridPos))
+            {
+                Debug.LogWarning("Skipping overlapping block " + waypoint);
+            }
+            else
+            {
+                grid.Add(gridPos, waypoint); //at each waypoints pos, add the waypoint itself
+            
+            }
+        }
+        
+    }
+}
--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -12,14 +12,20 @@ public class Waypoint : MonoBehaviour
     {
         return gridSize;
     }
-    public Vector2 GetGridPos()
+    public Vector2Int GetGridPos()
     {                          
         return new Vector2Int(    
             Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
             Mathf.RoundToInt(transform.position.z / gridSize) * gridSize
         );
     }
+    //the prev two lines were once in CubeEditor, but we moved it here so 
+    //waypoint is independant from CubeEditor. If we delete CubeEditor, the blue pill will still move
+
+    public void SetTopColor(Color color)
+    {
+        MeshRenderer topMeshRenderer = transform.Find("Top").GetComponent<MeshRenderer>(); //get Cube's top's mesh renderer
+        topMeshRenderer.material.color = color;
+    }
 }
 
-//the prev two lines were once in CubeEditor, but we moved it here so 
-//waypoint is independant from CubeEditor. If we delete CubeEditor, the blue pill will still move
