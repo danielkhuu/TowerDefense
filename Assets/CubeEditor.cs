159bb6e0c1164d890672f46c37f1fcb855e718a0
Daniel Khuu <danieldkhuu@gmail.com>
2020-06-22 16:06:19 -0500

    4_TD_UN2 Smart Blue Pill
    Blue pill smart enough to move using a foreach loop

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -15,13 +15,14 @@ public class CubeEditor : MonoBehaviour
     void Update()
     {
         Vector3 snapPos;
-        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) *gridSize;
-        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) *gridSize;
 
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) *gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
-        Debug.Log("Editor causes this Update");
 
         textMesh = GetComponentInChildren<TextMesh>(); //grabs textmesh from cube top
-        textMesh.text = snapPos.x +","+snapPos.z;
+        string labelText = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        textMesh.text = labelText;
+        gameObject.name = "Cube " + labelText;
     }
 }
---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField] List<Waypoint> path;
+    [SerializeField] float dwellTime = 1f;
+
+    void Start()
+    {
+        StartCoroutine(FollowPath()); //kind of like invokeRepeating but embedded
+    }
+
+    IEnumerator FollowPath() //for that to work ^ must use IEnumerator and return something
+    {
+        print("Starting patrol...");
+        foreach (Waypoint waypoint in path) //since path has all the cubes, Block waypoint is a cube
+        {
+            transform.position = waypoint.transform.position;
+            print("visiting block: " + waypoint);
+            yield return new WaitForSeconds(dwellTime);
+        }
+        print("Ending patrol.");
+    }
+}
