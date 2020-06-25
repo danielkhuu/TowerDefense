a4d6579b091642b3d1ceacd36bdd9cc8919b816d
Daniel Khuu <danieldkhuu@gmail.com>
2020-06-24 23:17:24 -0500

    14_TD_UN2 Before I revise coroutines
    For enemy spawning

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] EnemyMovement enemyToSpawn;
+    [SerializeField] float spawnDelay = 2f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
