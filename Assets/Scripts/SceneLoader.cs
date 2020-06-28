366c4059a2d81c54ba455b94367520045498128b
Daniel Khuu <danieldkhuu@gmail.com>
2020-06-27 19:12:16 -0500

    27_TD_UN2 Menu Screen

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void Start()
+    {
+        Invoke("InstructionsScene", 4f);
+    }
+    public void InstructionsScene()
+    {
+        SceneManager.LoadScene(1);
+        Time.timeScale = 1;
+    }
+
+}
